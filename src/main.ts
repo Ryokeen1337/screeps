3591283ffc0c915e438cd73837bfedcb5bb0e952
Ryokeen1337 <Ryokeen-NNK@hotmail.de>
2017-12-01 02:54:34 +0100

    -set initialize to true and use the gameServices class

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,34 @@
 import { creepFactory } from "./creeps/creepFactory";
 import { creepManager } from "./creeps/creepManager";
 import { gameServices } from "./gameServices";
+import { serviceNames } from "./common/serviceNames";
 
 let _isInitialized : boolean = false;
-let _creepManager : creepManager = null;
-let _creepFact : creepFactory = null;
 let _gameService : gameServices = null;
 
 function initialize(){
 	if(_isInitialized)
 		return;
 
-	if(_creepManager == null)
-		_creepManager = new creepManager();
-	if(_creepFact == null)
-		_creepFact = new creepFactory(_creepManager);
+	let _creepManager : creepManager = new creepManager();
+	let _creepFact : creepFactory = new creepFactory(_creepManager);
 
 	if(_gameService == null)
 		_gameService = new gameServices();
 
 	_gameService.registerService(_creepManager.getServiceName(),_creepManager);
 	_gameService.registerService(_creepFact.getServiceName(),_creepFact);
+
+	_isInitialized = true;
 }
 
 export const loop = function() 
 {
 	initialize();
 
+	let _creepFact : creepFactory = _gameService.getService(serviceNames.creepFactoryServ);
+	let _creepManager : creepManager = _gameService.getService(serviceNames.creepManagerServ);
+	
 	_creepFact.processQueue();
 	_creepManager.processCreeps();
 
